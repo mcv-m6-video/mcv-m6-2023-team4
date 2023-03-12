30c3fa785d5c28bb60cd492a7973e12d5598a23f
jblancoarnaus <j.blanco.arnaus@gmail.com>
2023-03-13 00:24:15 +0100

    dense opt flow + mse visualisation

--- Week 1/plot_optical_flow.py
+++ Week 1/plot_optical_flow.py
@@ -6,41 +6,34 @@ import sys
 from ipynb.fs.full.optical_flow_metrics import read_kitti_flow
 dataset_flow_path = '../datasets/data_stereo_flow/training/flow_noc/'
 
-filename = '000157_10.png'
+filename = '000045_10.png'
 
-dataset_flow_path = '../datasets/results/'
-filename = 'LKflow_000045_10.png'
+#dataset_flow_path = '../datasets/results/'
+#filename = 'LKflow_000045_10.png'
 filename_path = dataset_flow_path+filename
 dataset_image_path =  '../datasets/data_stereo_flow/training/image_0/'
+dataset_image_colored=  '../datasets/data_stereo_flow/training/colored_0/'
 image = '000045_10.png'
+
 plot_save_path = 'optflow.png'
 image_path = dataset_image_path+image
+image_colored = dataset_image_colored+image
 filename_path = dataset_flow_path+filename
 
 np.set_printoptions(threshold=sys.maxsize)
 
 
-def vector_to_rgb(angle, absolute):
-    #from https://stackoverflow.com/questions/19576495/color-matplotlib-quiver-field-according-to-magnitude-and-direction
-    global max_abs
-    # normalize angle
-    angle = angle % (2 * np.pi)
-    if angle < 0:
-        angle += 2 * np.pi
-
-    return matplotlib.colors.hsv_to_rgb((angle / 2 / np.pi, 
-                                         absolute / max_abs, 
-                                         absolute / max_abs))
 
 
-img = cv2.imread(image_path,cv2.IMREAD_UNCHANGED )
-u,v, valid_data= read_kitti_flow(filename_path)
-u = u*valid_data
-v = v*valid_data
-
-def plot_optical_flow_dense(u,v,plot_save_path):
+def plot_optical_flow_dense(u,v,img,plot_save_path):
+    #subsample using step_size
+    mask = np.zeros_like((img))
+    print(img.shape)
+    print("MASK ", mask.shape)
+    # Sets image saturation to maximum
+    mask[..., 1] = 255
     # Computes the magnitude and angle of the 2D vectors
-    magnitude, angle = cv2.cartToPolar(u, v)
+    magnitude, angle = cv2.cartToPolar(u, -v)
       
     # Sets image hue according to the optical flow 
     # direction
@@ -54,18 +47,22 @@ def plot_optical_flow_dense(u,v,plot_save_path):
     rgb = cv2.cvtColor(mask, cv2.COLOR_HSV2BGR)
       
     # Opens a new window and displays the output frame
-    cv2.imshow("dense optical flow", rgb)
-      
-    ax.grid(False)
-    # Hide axes ticks
-    #ax.set_xlim(right = u.shape[1])
-    #ax.set_ylim(bottom = u.shape[0])
-    ax.set_xticks([])
-    ax.set_yticks([])
-    plt.savefig(plot_save_path, bbox_inches='tight', pad_inches=0, dpi = 200)
-plot_optical_flow_color(u,v,img,plot_save_path) 
+    cv2.imwrite("dense"+plot_save_path, rgb)
   
+
+global max_abs
 def plot_optical_flow_color(u,v,img,plot_save_path):
+    def vector_to_rgb(angle, absolute):
+        #from https://stackoverflow.com/questions/19576495/color-matplotlib-quiver-field-according-to-magnitude-and-direction
+
+        # normalize angle
+        angle = angle % (2 * np.pi)
+        if angle < 0:
+            angle += 2 * np.pi
+
+        return matplotlib.colors.hsv_to_rgb((angle / 2 / np.pi, 
+                                            absolute / max_abs, 
+                                            absolute / max_abs))
     #subsample using step_size
     step_size =10
     u_ssampled = u[::step_size, ::step_size]
@@ -77,7 +74,7 @@ def plot_optical_flow_color(u,v,img,plot_save_path):
     ny = u_ssampled.shape[0]
     X, Y = np.meshgrid(np.linspace(1, u.shape[1],nx),np.linspace(1, u.shape[0],ny))
 
-    maxOF = max(np.max(u), np.max(v))
+    max_uv = max(np.max(u), np.max(v))
     angles = np.arctan2(v_ssampled, u_ssampled)
     lengths = np.sqrt(np.square(u_ssampled) + np.square(v_ssampled))
     max_abs = np.max(lengths)
@@ -89,7 +86,7 @@ def plot_optical_flow_color(u,v,img,plot_save_path):
     fig, ax = plt.subplots(figsize=(40,40))
     ax.axis("off")
     ax.imshow(img, cmap='gray')
-    q = ax.quiver(X,Y, u_ssampled, -v_ssampled,scale=maxOF*8,color = c,width=0.001)  #kitti's coordinate origin is flipped for the y coordinate
+    q = ax.quiver(X,Y, u_ssampled, -v_ssampled,scale=max_uv*8,color = c,width=0.001)  #kitti's coordinate origin is flipped for the y coordinate
 
     # Hide grid lines
     ax.grid(False)
@@ -99,4 +96,13 @@ def plot_optical_flow_color(u,v,img,plot_save_path):
     ax.set_xticks([])
     ax.set_yticks([])
     plt.savefig(plot_save_path, bbox_inches='tight', pad_inches=0, dpi = 200)
+    
 
+img = cv2.imread(image_path,cv2.IMREAD_UNCHANGED )
+print("PATH ", image_colored)
+img_color = cv2.imread(image_colored,cv2.IMREAD_UNCHANGED )
+u,v, valid_data= read_kitti_flow(filename_path)
+u = u*valid_data
+v = v*valid_data
+plot_optical_flow_dense(u,v,img_color,plot_save_path) 
+#plot_optical_flow_color(u,v,img,plot_save_path) 
